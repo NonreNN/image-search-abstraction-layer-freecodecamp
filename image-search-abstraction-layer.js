158969be1567f34cb73bc5703521584810dfa91c
Manuel Fernando Forero Reyes <nonre.nn@gmail.com>
2017-10-02 13:48:00 +0200

    upload

---
+++ image-search-abstraction-layer.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+const googleAPIKey = process.env.GOOGLE
+
+let recentlySearch = []
+
+app.get('/api/imagesearch/:search', function(req, res) {
+    recentlySearch.push(req.params.search);
+    let search = `https://www.googleapis.com/customsearch/v1?key=${googleAPIKey}&cx=012086283050852391430:eybjsxiukte&searchType=image&start=${req.query.offset}&q=${req.params.search}`
+    request(search, function (error, response, body) {
+        let bodyJSON = JSON.parse(body);
+        let completed = []
+        bodyJSON.items.forEach(function(element) {
+            let obj = {url : element.link, snippet : element.snippet, thumbnail : element.image.thumbnailLink, context : element.image.contextLink}
+            completed.push(obj)
+        }, this);
+        res.json(completed)         
+    });   
+});
+
+app.get('/api/recently', function(req, res) {
+    res.json({recently : recentlySearch});
+});
+
+app.listen(process.env.PORT || 3000);
